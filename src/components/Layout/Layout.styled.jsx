dcbad748213fafa342949f97fd1659021c029cb7
Marvin Pacis <108392678+marventures@users.noreply.github.com>
2024-09-23 22:06:49 +0800

    refactor: adjust margins and paddings

--- src/components/Layout/Layout.styled.jsx
+++ src/components/Layout/Layout.styled.jsx
@@ -4,7 +4,7 @@ export const Container = styled.div`
   background-color: ${({ darkMode }) => (darkMode ? '#121212' : '#f9f9f9')};
   color: ${({ darkMode }) => (darkMode ? '#ffffff' : '#000000')};
   min-height: 100vh;
-  padding: 20px;
+  padding: 10px 120px;
   transition: background-color 0.3s, color 0.3s;
 `;
 
@@ -12,5 +12,4 @@ export const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 20px;
 `;
